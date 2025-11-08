61b4ab07824dc587773f3a9bac4295d19a5da25a
PrzemoWAkcji <przemyslawnorbertjedrzejewski@gmail.com>
2025-11-08 11:47:36 +0100

    "remove-password-min-length"

--- athletics-platform/backend/src/auth/dto/login.dto.ts
+++ athletics-platform/backend/src/auth/dto/login.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
   @IsEmail({}, { message: 'Podaj prawidłowy adres email' })
@@ -7,6 +7,5 @@ export class LoginDto {
 
   @IsString({ message: 'Hasło musi być tekstem' })
   @IsNotEmpty({ message: 'Hasło jest wymagane' })
-  @MinLength(6, { message: 'Hasło musi mieć co najmniej 6 znaków' })
   password: string;
 }
